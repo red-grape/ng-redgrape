1884b06b4e10f6f1a514f50c05292f56f66218cf
red-grape <theredgrape96@gmail.com>
2026-02-13 11:07:55 +0330

    fix #7

--- projects/ng-redgrape-ui/src/lib/components/rg-main-page/rg-main-page.component.spec.ts
+++ projects/ng-redgrape-ui/src/lib/components/rg-main-page/rg-main-page.component.spec.ts
@@ -1,23 +1,66 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, DebugElement } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { MenuItem, RgMainPageComponent } from "ng-redgrape-ui"
+import { RgSideMenuComponent } from "../rg-side-menu/rg-side-menu.component";
+import { By } from "@angular/platform-browser";
 
-import { RgMainPageComponent } from './rg-main-page.component';
 
-describe('RgMainPageComponent', () => {
-  let component: RgMainPageComponent;
+describe("RgMainPageComponent Tests", () => {
+  let component : RgMainPageComponent;
   let fixture: ComponentFixture<RgMainPageComponent>;
+  let debugElement: DebugElement;
 
-  beforeEach(async () => {
+  beforeEach(async ()=>{
     await TestBed.configureTestingModule({
-      imports: [RgMainPageComponent]
-    })
-    .compileComponents();
+      imports : [RgSideMenuComponent]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(RgMainPageComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
     fixture.detectChanges();
-  });
+  })
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  it('should set instance correctly', () =>{
+    expect(component).not.toBeNull()
+  })
+
+  it("Should 'mobileView' be false at the beginning" , () => {
+    expect(component.mobileView).toBeFalse();
+  })
+
+  it("Should 'sideMenuCollapsed' be false at the beginning" , () => {
+    expect(component.sideMenuCollapsed).toBeFalse();
+  })
+
+  it("should 'menuItems' be empty", () => {
+    expect(component.menuItems.length).toBe(0);
+  })
+
+  it('should sideMenuCollapsed toggle when call toggleSideMenu method' , () => {
+    component.toggleSideMenu();
+    expect(component.sideMenuCollapsed).toBeTrue();
+  })
+
+  it("should 'menuItems' has length 1 when add 1 item", () => {
+    let menuItem = <MenuItem>{
+      text : "sample test",
+      children: [],
+      expanded: false,
+      icon: "icon path",
+      link: "link",
+      tag: {value: "sample"}
+    };
+    component.menuItems.push(menuItem);
+
+    expect(component.menuItems.length).toBe(1)
+  })
+
+  it("shold page will correct render wehen change 'sideMenuCollapsed'" , () => {
+    component.sideMenuCollapsed = true;
+    fixture.detectChanges();
+    let element = debugElement.query(By.css('.main-container'));
+    expect(element.classes['menu-collapsed']).toBeTruthy();
+    
+  })
+})
